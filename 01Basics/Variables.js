55068a10a18b056bcf7517a191d0ea2d2cc2e2a5
utsav kumar <utsavkumar5462@gmail.com>
2023-05-27 20:50:29 +0530

    Variables

---
+++ 01Basics/Variables.js
@@ -0,0 +1,24 @@
+let name = 'Utsav';
+let score = 103;
+let bonus = 20;
+
+let totalScore = score + bonus;
+console.log(name);
+console.log(totalScore);
+
+let firstName = "Utsav";
+let lastName = "Kumar";
+console.log(firstName + ' ' + lastName);
+
+firstName = "Light";
+lastName = "64";
+console.log(firstName + ' ' + lastName);
+
+const age = 21;
+console.log('Age' + ' ' + age);
+
+age = 26;
+// console.log('Age' + ' ' + age); assignment to constant variable is not allowed.
+
+
+
