187cc7846bdf95edc4db9c079f3c41b2e09c3549
utsav kumar <utsavkumar5462@gmail.com>
2023-05-27 21:15:42 +0530

    boolean

---
+++ 01Basics/NullAndBool.js
@@ -0,0 +1,12 @@
+var temp = 0;
+console.log("Current temperature is: " + temp);
+var temperature;
+console.log("Current temperature is: " + temperature);
+temperature = null;
+console.log("Current temperature is: " + temperature);
+
+let actualMarks = -7;
+let myGrade = actualMarks > 10;
+console.log(myGrade);
+myGrade = actualMarks < 10;
+console.log(myGrade);
