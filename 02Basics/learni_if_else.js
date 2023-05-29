8698488c06f89b9bcb85c0577fdc1194268e5236
utsav kumar <utsavkumar5462@gmail.com>
2023-05-30 00:54:33 +0530

    if_else

---
+++ 02Basics/learni_if_else.js
@@ -0,0 +1,20 @@
+// if (true) {
+//   console.log("Hey");
+// } else if (false ) {
+//   console.log("Hello");
+// } else {
+//   console.log("Hola");
+// }
+
+var whoIsHere = "abc";
+
+if (whoIsHere === "user") {
+  console.log("Greeting message for user");
+  console.log("Allow access to view all courses");
+} else if (whoIsHere === "teacher") {
+  console.log("Greeting message for teacher");
+  console.log("Allow access to view his courses");
+} else {
+  console.log("Message: please verify your email");
+  console.log("Send user an email for varification");
+}
