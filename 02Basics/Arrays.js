b95d9b90731dbaa5f6b22b1ecfded05d1dd377d1
utsav kumar <utsavkumar5462@gmail.com>
2023-06-26 12:59:35 +0530

    Marvel superHeroes in Arrays + dolar{}

---
+++ 02Basics/Arrays.js
@@ -0,0 +1,15 @@
+// datatype - Array
+
+let superHeroes = ["Iron man", "Superman", "capt. America"];
+console.log(superHeroes);
+console.log(superHeroes[0]);
+console.log(superHeroes[2]);
+console.log(superHeroes[5]);
+console.log(superHeroes.length);
+console.log(superHeroes[superHeroes.length]);
+console.log(superHeroes[superHeroes.length - 1]);
+// console.log("We have 3 super Heroes");
+// console.log(`We have ${3 + 4} super Heroes`);
+console.log(`We have ${superHeroes.length} super Heroes`);
+superHeroes = ["Iron man", "Superman", "capt. America", "Thor"];
+console.log(`We have ${superHeroes.length} super Heroes`);
