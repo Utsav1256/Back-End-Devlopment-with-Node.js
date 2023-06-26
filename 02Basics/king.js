64da9d4ccdfae2414022f877da863d7040589a5c
utsav kumar <utsavkumar5462@gmail.com>
2023-06-26 12:31:43 +0530

    Kings Problem

---
+++ 02Basics/king.js
@@ -0,0 +1,15 @@
+// Kings problem
+
+let king = "Ram";
+if (true) {
+  let king = "Bharat";
+  if (true) {
+    Kingo = "Lakshman";
+    console.log(Kingo);
+    console.log(king);
+  }
+}
+if (true) {
+  console.log("I am the second Kingo" + Kingo);
+  console.log("I am the second King" + king);
+}
