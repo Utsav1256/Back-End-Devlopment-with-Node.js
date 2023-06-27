63e7e1798643507f99f41e72189c9e9ac0b687ee
utsav kumar <utsavkumar5462@gmail.com>
2023-06-27 10:44:18 +0530

    Assignment1

---
+++ 01Intermediate/Assignment1.js
@@ -0,0 +1,17 @@
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+months.forEach(function (month, index) {
+  console.log(`Starts with ${month} -- ${index + 1}`);
+});
