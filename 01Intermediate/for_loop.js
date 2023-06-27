225d7d4b08263554b122be6f265a8321b001be5d
utsav kumar <utsavkumar5462@gmail.com>
2023-06-27 11:07:20 +0530

    for loop

---
+++ 01Intermediate/for_loop.js
@@ -0,0 +1,9 @@
+let Todos = ["exercise", "brush", "bath"];
+
+for (let index = 0; index < Todos.length; index++) {
+  console.log(Todos[index]);
+}
+console.log("-----------");
+for (let i = Todos.length - 1; i >= 0; i--) {
+  console.log(Todos[i]);
+}
